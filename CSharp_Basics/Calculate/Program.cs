c36fa56a8b01c745f9027420d09664625cd8a261
GosiaS <gosiunia_1@wp.pl>
2019-09-26 10:34:55 +0200

    switche

--- CSharp_Basics/Calculate/Program.cs
+++ CSharp_Basics/Calculate/Program.cs
@@ -18,7 +18,14 @@ namespace Calculate
 
             Console.WriteLine("Podaj liczbę pierwszą:");
             string number1 = Console.ReadLine();
-            int parsedNumber = int.Parse(number1);
+            //zwraca czy udało się sparsować czy nie, jest typu bool, czyli najpierw zobacz czy się da i jak tak to parsuj
+            //a ta druga linijka tylko parsuje
+            int parsedNumber = 0;
+            if (!int.TryParse(number1, out parsedNumber))
+            {
+                Console.WriteLine($"nieznana wartość używamy wartości domyślnej {parsedNumber}");
+            }
+            // int parsedNumber = int.Parse(number1);
 
             Console.WriteLine("Podaj liczbę drugą:");
             string number2 = Console.ReadLine();
@@ -34,24 +41,24 @@ namespace Calculate
             //przypisanie przed pętlą bo inaczej nie będzie wiedzial poza pętlą, a tak to w pętli tylko zmieniamy tą wartość
             double result = 0;
 
-            if (dzialanie == "+")
-            {
-                result = Add(parsedNumber, parsedNumber2);
-            }
-            else if (dzialanie == "*")
-            {
-                result = Multiply(parsedNumber, parsedNumber2);
-            }
-            else if (dzialanie == "/")
-            {
-                result = Divide(parsedNumber, parsedNumber2);
-            }
-            else if (dzialanie == "-")
-            {
-                result = Substract(parsedNumber, parsedNumber2);
-            }
-            else
-                Console.WriteLine("Podałeś błędny znak działania");
+            //if (dzialanie == "+")
+            //{
+            //    result = Add(parsedNumber, parsedNumber2);
+            //}
+            //else if (dzialanie == "*")
+            //{
+            //    result = Multiply(parsedNumber, parsedNumber2);
+            //}
+            //else if (dzialanie == "/" && parsedNumber2 != 0)
+            //{
+            //    result = Divide(parsedNumber, parsedNumber2);
+            //}
+            //else if (dzialanie == "-")
+            //{
+            //    result = Substract(parsedNumber, parsedNumber2);
+            //}
+            //else
+            //    Console.WriteLine("Podałeś błędny znak działania");
         
     
 
@@ -63,9 +70,41 @@ namespace Calculate
 
          //   Console.WriteLine($"dodawanie {result}, dzielenie {divide}, mnozenie {multiply}, odejmowanie {substract}");
            
-            
-
             Console.ReadKey();
+
+
+            switch (dzialanie)
+            {
+                case "+":
+                    result = Add(parsedNumber, parsedNumber2);
+                    break;
+
+                case "-":
+                    result = Substract(parsedNumber, parsedNumber2);
+                    break;
+
+                case "*":
+                case "x":
+                    result = Multiply(parsedNumber, parsedNumber2);
+                    break;
+
+                case "/":
+                    if (parsedNumber2 == 0)
+                    {
+                        Console.WriteLine("Nie dziel przez zero");
+                        break;
+                    }
+                    result = Divide(parsedNumber, parsedNumber2);
+                    break;
+
+                default:
+                    Console.WriteLine("Nie znaleziono symbolu");
+                    break;
+            }
+
+            Console.WriteLine($"{result}");
+            Console.ReadLine();
+
         }
 
         public static int Add(int firstNumber, int secondNumber)
@@ -76,7 +115,7 @@ namespace Calculate
 
         public static double Divide(int firstNumber, int secondNumber)
         {
-            double div = firstNumber / secondNumber;
+            double div = (double)firstNumber / (double)secondNumber;
             return div;
         }
 
