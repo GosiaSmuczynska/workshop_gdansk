ac99b68aa7378a459d7ceaa91528309a42f3b8a6
GosiaS <gosiunia_1@wp.pl>
2019-09-27 08:28:20 +0200

    nowyt selenium project

---
+++ CSharp_Basics/SeleniumTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace SeleniumTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            new ChromeDriver();
+        }
+    }
+}
---
+++ CSharp_Basics/SeleniumTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbf5f3bf-3138-452e-ae08-5b2e195d00a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
