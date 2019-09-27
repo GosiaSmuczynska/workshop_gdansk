fcbf6ad38c17f00447da685a27bf87db9239bf69
GosiaS <gosiunia_1@wp.pl>
2019-09-27 11:53:04 +0200

    testy contact us

--- CSharp_Basics/SeleniumTest/ContactUsTest.cs
+++ CSharp_Basics/SeleniumTest/ContactUsTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace SeleniumTest
 {
-    class ContactUsTest
+    [TestFixture]
+
+    class ContactUsTest : TestBase
     {
+
+        [SetUp]
+        // najpierw wykona setup w klasie bazowej i dopiero w naszej klasie
+        //czyli tu możemy wpisać inny url do wszystkich testów poniżej
+
+        // driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+        [Test]
+        public void ContactError()
+        {
+
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            var contactHeader = driver.FindElement(By.CssSelector("#contact-link"));  //ten element musimy do czegoś przypisać
+            contactHeader.Click();
+
+            driver.FindElement(By.CssSelector("#submitMessage")).Click();
+
+            WebDriverWait waitDriver = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            waitDriver.Until(ExpectedConditions.ElementExists(By.CssSelector(".alert-danger")));  //będzie czekać 15 sekund aż nie znajdzie tego elementu, to samo co implicit ale lokalnie plus możemy czekać aż np się nie pojawi - bardziej rozbudowany
+
+            var ifError = driver.FindElement(By.CssSelector(".alert-danger"));  //można dodać li jeśli chcemy się dostać do elementu w środku czyli invalid email adres
+            string text = ifError.Text;
+            
+            StringAssert.Contains("error", text);
+
+            Assert.IsTrue(ifError.Displayed, "Error message wasn't displayed to user");
+
+            
+        }
     }
 }
--- CSharp_Basics/SeleniumTest/TestBase.cs
+++ CSharp_Basics/SeleniumTest/TestBase.cs
@@ -11,13 +11,15 @@ namespace SeleniumTest
 {
     public class TestBase
     {
-        public IWebDriver driver;
+        protected IWebDriver driver;  //teraz ten driver będzie widoczny dla innych metod w teście, tu już deklarujemy ją i dlatego na dole już kasujemy var driver tylko przypisujemy do driver
+        // jeste też protected: i ta klasa która dziedziczy jest dla niej ok, tylko dzieci mogą się do niej dostać
+        //public widza wszyscy
 
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();  // tworzy obiekt tego drivera, postaw server tego drivera, skoro we wszystkich testach będziemy używać tego driver to inicjalizujemy go poza testem
-            var waitTime = new System.TimeSpan(0, 0, 1);
+            var waitTime = new System.TimeSpan(0, 0, 8);
             driver.Manage().Timeouts().ImplicitWait = waitTime;
         }
 
