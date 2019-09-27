05e2233b735f13fa1a848c7a09e6bb0112fb57c3
GosiaS <gosiunia_1@wp.pl>
2019-09-27 14:31:22 +0200

    nowe kategorie z pageCategories

---
+++ CSharp_Basics/SeleniumTest/CategoriesPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTest
+{
+    public class CategoriesPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait webDriverWait;
+        private By categoryHeaderLocator = By.CssSelector("ul.sf-menu > li > a[title='Dresses']");
+        private By productCounterLocator = By.CssSelector(".heading-counter");
+
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+
+        public CategoriesPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            webDriverWait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+        }
+
+        public void ClickHeaderButton()
+        {
+            driver.FindElement(categoryHeaderLocator).Click();
+        }
+
+        public string ProductCounter()
+        {
+            var count = driver.FindElement(productCounterLocator);
+            string counter = count.Text;
+            return counter;
+        }
+    }
+}
--- CSharp_Basics/SeleniumTest/CategoriesTest.cs
+++ CSharp_Basics/SeleniumTest/CategoriesTest.cs
@@ -9,6 +9,7 @@ namespace SeleniumTest
     [TestFixture]
     public class CategoriesTest : TestBase  //po dwukropku wpisujemy klasę po której dziedziczymy i ta nasza będzie widziała te cechy ale dzieci nie mają dostępu do prywatnych rzeczy swoich rodziców więc w rodzicu musi być driver jako public
     {
+        CategoriesPage sut;
       //  IWebDriver driver;  //teraz ten driver będzie widoczny dla innych metod w teście, tu już deklarujemy ją i dlatego na dole już kasujemy var driver tylko przypisujemy do driver
               //[TestCase("ul.sf-menu > li > a[title='Dresses']")]
         //public void CheckingProductNumber(string element, string productCounter)
@@ -19,18 +20,29 @@ namespace SeleniumTest
         //    string text = productCounter.Text;
         //    StringAssert.Contains("5", text);
         //}
+        [SetUp]
+        public void CategoriesSetup()
+        {
+            sut = new CategoriesPage(driver);
+            sut.GoToPage();
+        }
 
         [Test]
         public void FirstTest()
         {
 
-            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+         //   driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
 
-            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='Dresses']"));  //ten element musimy do czegoś przypisać
+          //  var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='Dresses']"));  //ten element musimy do czegoś przypisać
+            sut.ClickHeaderButton();
+
             //element.jedna z klas > weź bezpośrednie dziecko
-            categoryHeader.Click();
-            var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
-            string text = productCounter.Text;
+            // categoryHeader.Click();
+
+            var productCount = sut.ProductCounter();
+
+           // var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
+          //  string text = productCounter.Text;
             var webElements = driver.FindElements(By.CssSelector("div.product-container")); //mamy kolekcje i możemy z niego brać numery z danej kolekcji
 
             string actualElementCount = webElements.Count.ToString();
@@ -38,7 +50,7 @@ namespace SeleniumTest
 
             // text.Contains("5");  albo lepiej funkcją która obsługuje stringi
             // StringAssert.Contains("5", text);
-            StringAssert.Contains(actualElementCount, text);
+            StringAssert.Contains(actualElementCount, productCount);
 
 
 
